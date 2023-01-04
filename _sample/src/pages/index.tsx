2fc34a7115bfa30f4df2d77ec657effe3959f957
otaki0413 <takida0413@gmail.com>
2023-01-04 19:31:54 +0900

    ✨ 講座開始の下準備

--- _sample/src/pages/index.tsx
+++ _sample/src/pages/index.tsx
@@ -1,7 +1,36 @@
 import type { NextPage } from "next";
 
+const TODOS = [
+  {
+    id: 1,
+    text: "test1",
+    isDone: false,
+  },
+  {
+    id: 2,
+    text: "test2",
+    isDone: true,
+  },
+];
+
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h3>TODO一覧</h3>
+      {TODOS.map((todo) => (
+        <div key={todo.id}>
+          <label style={{ fontSize: "2rem" }}>
+            <input
+              type="checkbox"
+              checked={todo.isDone}
+              style={{ width: "1.5rem", height: "1.5rem" }}
+            />
+            {todo.text}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;

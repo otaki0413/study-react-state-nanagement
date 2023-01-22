05685a5eb63997a223f6a9aff3ff3f00f7eee2d3
otaki0413 <takida0413@gmail.com>
2023-01-22 22:55:04 +0900

    ✨ Jotai01の完了

---
+++ Jotai01/src/components/TodoCounter.tsx
@@ -0,0 +1,9 @@
+import { useAtom } from "jotai";
+import { FC } from "react";
+import { todosLengthAtom } from "src/state/todo";
+
+export const TodoCounter: FC = ({}) => {
+  const [todosLength] = useAtom(todosLengthAtom);
+
+  return <h2>TODO: {todosLength}件</h2>;
+};
---
+++ Jotai01/src/state/todo.ts
@@ -0,0 +1,46 @@
+import { atom } from "jotai";
+import { selectAtom } from "jotai/utils";
+import { Todo } from "src/types";
+
+export const todosAtom = atom<Todo[]>([
+  {
+    id: 1,
+    text: "test1",
+    isDone: false,
+  },
+  {
+    id: 2,
+    text: "test2",
+    isDone: true,
+  },
+]);
+
+export const todosLengthAtom = selectAtom(todosAtom, (todos) => todos.length);
+
+export const toggleTodoAtom = atom<Todo[], Pick<Todo, "id">>(
+  (get) => get(todosAtom),
+  (get, set, update) => {
+    const prevTodos = get(todosAtom);
+    const newTodos = prevTodos.map((todo) => {
+      if (todo.id === update.id) {
+        return { ...todo, isDone: !todo.isDone };
+      }
+      return todo;
+    });
+    set(todosAtom, newTodos);
+  }
+);
+
+export const addTodoAtom = atom<null, Pick<Todo, "text">>(
+  null,
+  (get, set, update) => {
+    const prevTodos = get(todosAtom);
+    const newTodo = {
+      id: prevTodos.length + 1,
+      text: update.text,
+      isDone: false,
+    };
+    const newTodos = [...prevTodos, newTodo];
+    set(todosAtom, newTodos);
+  }
+);
